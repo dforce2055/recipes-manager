cddcbc47ece15056587ccbc4c6ecbdd5052cfca5
Diego Pérez <dperez2055@gmail.com>
2022-06-09 10:53:13 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Recipes Manager</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/interfaces.ts
@@ -0,0 +1,6 @@
+export interface Card {
+  id: string | number
+  src: string,
+  matched: boolean,
+  disabled: boolean
+}
